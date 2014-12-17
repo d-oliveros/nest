f148a31f2ee2ce764d74b31630c7842142a0e43e
David Oliveros <dato.oliveros@gmail.com>
2014-12-17 03:00:33 -0600

    Spacing issue

--- test/index.js
+++ test/index.js
@@ -7,6 +7,6 @@ require(__database  +'/Operation/test');
 require(__framework +'/Agent/test');
 require(__framework +'/Route/test');
 
-require(__routes  +'/test');
+require(__routes    +'/test');
 
 require(__framework +'/engine/test');

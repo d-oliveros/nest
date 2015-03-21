54ca7f648e03aa2467653d87d5a275f9c4ef4170
MrOutis <mister.outis@hotmail.com>
2015-03-20 21:50:09 -0600

    Add sinembargo (newspaper)

---
+++ routes/sinembargo/init.js
@@ -0,0 +1,16 @@
+
+// Exports: Scraping initialization script
+//
+// Starts extracting data from sinembargo
+// by searching the word "taco", and spawning crawlers to the posts
+// 
+exports.start = function() {
+	console.log('Initializing sinembargo...');
+
+	var searchRoute = require('./search');
+	var engine = require('../../lib/engine');
+	
+	searchRoute.initialize('taco', function() {
+		engine.start();
+	});
+};
